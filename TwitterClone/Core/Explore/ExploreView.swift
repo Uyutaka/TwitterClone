85a6df1902fedd058238bf4abeee65556f16b46a
uyutaka <yutaka.urakami@colorado.edu>
2022-10-12 16:16:59 +0900

    [Main Tab Setup] finish 28:39

---
+++ TwitterClone/Core/Explore/ExploreView.swift
@@ -0,0 +1,20 @@
+//
+//  ExploreView.swift
+//  TwitterClone
+//
+//  Created by Yutaka Urakami on 2022/10/12.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text("Explore View")
+    }
+}
+
+struct ExploreView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExploreView()
+    }
+}
---
+++ TwitterClone/Core/Notifications/NotificatinsView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificatinsView.swift
+//  TwitterClone
+//
+//  Created by Yutaka Urakami on 2022/10/12.
+//
+
+import SwiftUI
+
+struct NotificatinsView: View {
+    var body: some View {
+        Text("Notifications View")
+    }
+}
+
+struct NotificatinsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificatinsView()
+    }
+}
