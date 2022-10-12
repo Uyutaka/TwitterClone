f531a51d0e7291d1f7bcbf4f4796ed49d97d7fb1
uyutaka <yutaka.urakami@colorado.edu>
2022-10-12 15:27:42 +0900

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Yutaka Urakami on 2022/10/12.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
